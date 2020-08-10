6bd947ec68b506f61c01835e6d8b2441d731005b
unknown <ribonucleasa@gmail.com>
2020-08-10 21:29:40 +0200

    Add callback support; add 'next scene' callback Next().

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { ScalingCanvas } from "@azleur/scaling-canvas";
 import { Rect } from "@azleur/rect";
 
+/** Callback wrapper passed to the Update() function from the running game loop. */
+export type SceneCallbacks = {
+    Next: (nextScene: Scene) => void,
+};
+
 export type Scene = {
     World: Rect,
-    Update: (dT: number) => void,
+    Update: (dT: number, callbacks: SceneCallbacks) => void,
     Render: (canvas: ScalingCanvas) => void,
 };
 
 export const Start = (canvasId: string, scene: Scene): void => {
+
+    const callbacks: SceneCallbacks = {
+        Next: (nextScene: Scene): void => {
+            scene = nextScene;
+        },
+    };
 
     const rawCanvas = document.getElementById(canvasId);
     if(!(rawCanvas instanceof HTMLCanvasElement)) {
@@ -21,10 +32,10 @@ export const Start = (canvasId: string, scene: Scene): void => {
 
     const tick = (millis: number) => {
         const newTime = millis / 1000;
-        let dT = time - newTime;
+        let dT = newTime - time;
         time = newTime;
 
-        scene.Update(dT);
+        scene.Update(dT, callbacks);
         scene.Render(canvas);
 
         requestAnimationFrame(tick);
--- test/src/game.ts
+++ test/src/game.ts
@@ -1,33 +1,49 @@
 import { Vec2 } from "@azleur/vec2";
 import { FromCenterRadius } from "@azleur/rect";
 import { ScalingCanvas } from "@azleur/scaling-canvas";
-import { Scene, Start } from "@azleur/game-loop";
+import { Scene, Start, SceneCallbacks } from "@azleur/game-loop";
 
 window.onload = () => {
 
-    const MakeScene = (): Scene => {
-        console.log(Vec2.Zero);
-        console.log(FromCenterRadius(Vec2.Zero, 2));
-        const World = FromCenterRadius(Vec2.Zero, 2);
-        console.log(World);
+    const World = FromCenterRadius(Vec2.Zero, 2);
+
+    const Scene_1 = (): Scene => {
         let pos = Vec2.Right;
         let vel = Vec2.Up;
-
-        console.log(World, pos, vel);
+        let totalTime = 0;
 
         return {
             World,
-            Update: (dT: number) => {
-                const accel = pos.Negate();
-                vel = vel.Add(accel.Times(dT));
-                pos = pos.Add(vel.Times(dT));
+            Update: (dT: number, callbacks: SceneCallbacks) => {
+                totalTime += dT;
+                console.log(totalTime);
+
+                if(totalTime < 1) {
+                    const accel = pos.Negate();
+                    vel = vel.Add(accel.Times(dT));
+                    pos = pos.Add(vel.Times(dT));
+                } else {
+                    console.log("Change scene");
+                    callbacks.Next(Scene_2());
+                }
             },
-            Render: (canvas: ScalingCanvas) => { 
+            Render: (canvas: ScalingCanvas): void => {
+                
                 canvas.Clear("white");
                 canvas.FillCircle(pos, 0.1, { brush: "blue" });
             },
         };
     };
 
-    Start("my-canvas", MakeScene());
+    const Scene_2 = (): Scene => {
+        return {
+            World,
+            Update: (dT: number, callbacks: SceneCallbacks): void => {},
+            Render: (canvas: ScalingCanvas): void => {
+                canvas.Clear("red");
+            },
+        };
+    };
+
+    Start("my-canvas", Scene_1());
 }
